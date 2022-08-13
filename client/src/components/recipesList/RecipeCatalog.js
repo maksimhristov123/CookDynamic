eb223b0522fd1c7e2f62a0a57acd6506903ec9d2
Maksim Hristov <maxim.1990@abv.bg>
2022-08-13 08:08:19 +0300

    No recipes in catalog

--- client/src/components/recipesList/RecipeCatalog.js
+++ client/src/components/recipesList/RecipeCatalog.js
@@ -17,27 +17,32 @@ export const RecipeCatalog = (props) => {
     return (
         <div className="recipes_menu_container">
 
-            {!props.userId ?
-                recipes.map((recipe) => <RecipeItem
-                    key={recipe._id}
-                    recipeId={recipe._id}
-                    recipeImage={recipe.recipeImage}
-                    cookTime={recipe.recipeTime}
-                    category={recipe.recipeCategories}
-                    recipeTitle={recipe.recipeTitle}
-                    resipeDescription={recipe.recipeDescription}
-                />)
-                : recipes.filter((recipe) => recipe.recipeAuthor === props.userId)
-                    .map((r) => <RecipeItem
-                        key={r._id}
-                        recipeId={r._id}
-                        recipeImage={r.recipeImage}
-                        cookTime={r.recipeTime}
-                        category={r.recipeCategories}
-                        recipeTitle={r.recipeTitle}
-                        resipeDescription={r.recipeDescription}
+            {recipes.length === 0 ?
+                <p>There is no recipes!</p>
+                : !props.userId ?
+                    recipes.map((recipe) => <RecipeItem
+                        key={recipe._id}
+                        recipeId={recipe._id}
+                        recipeImage={recipe.recipeImage}
+                        cookTime={recipe.recipeTime}
+                        category={recipe.recipeCategories}
+                        recipeTitle={recipe.recipeTitle}
+                        resipeDescription={recipe.recipeDescription}
                     />)
+                    : recipes.filter((recipe) => recipe.recipeAuthor === props.userId)
+                        .map((r) => <RecipeItem
+                            key={r._id}
+                            recipeId={r._id}
+                            recipeImage={r.recipeImage}
+                            cookTime={r.recipeTime}
+                            category={r.recipeCategories}
+                            recipeTitle={r.recipeTitle}
+                            resipeDescription={r.recipeDescription}
+                        />)
+                
             }
+
+           
 
         </div>
     )
